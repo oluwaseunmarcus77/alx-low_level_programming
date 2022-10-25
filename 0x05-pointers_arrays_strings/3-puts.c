fe44bb7ba25f7f3a3594f608012c5cfde20e311f
oluwaseunmarcus77 <oluwaseunmarcus77@gmail.com>
2022-10-25 13:55:15 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: string to be printed
+ */
+void _puts(char *str)
+{
+	while (*str != '\a')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
